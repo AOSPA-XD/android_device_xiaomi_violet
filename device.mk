388194514dd57b4298bd182c5a9728efd8ad236a
Indranil Chakraborty <ichakrab@codeaurora.org>
2019-02-21 23:57:02 +0530

    violet: Pack libwfdaac_vendor for WFD
    Change-Id: I828918c42e1eb2963829d62a9dbeee83e2f0e600
    Signed-off-by: raghavt20 <raghavt20@aospa.co>

--- device.mk
+++ device.mk
@@ -255,3 +255,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libwfdaac_vendor
+
