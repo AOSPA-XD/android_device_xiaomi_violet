99e010440cf7435523b5c12e9c6e495a888ecbd3
Arian <arian.kulmer@web.de>
2021-10-11 11:06:41 +0200

    violet: Provide bluetooth and gatekeeper interfaces on vendor
    Change-Id: I482503cafaab16d50241ad90b1f51e882727c6f2

--- device.mk
+++ device.mk
@@ -88,6 +88,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
@@ -172,6 +173,10 @@ PRODUCT_PACKAGES += \
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor
 
 # GPS
 PRODUCT_PACKAGES += \
