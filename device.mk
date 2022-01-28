f72918e43a54ea87778cc53338bffb7ba570db18
raghavt20 <raghavt20@aospa.co>
2022-01-28 14:54:15 +0000

    [DNM]violet: Don't compile FM radio
    - AOSPA's FM radio crashes when opened

--- device.mk
+++ device.mk
@@ -131,7 +131,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # FM
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
