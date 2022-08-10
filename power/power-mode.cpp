ZChrySA/wave

violet: power: Add NVTCapacitiveTouchScreen support
Change-Id: Ifd151f0642b495f80cc512d4fe19d1cfab97bcf8

power/power-mode.cpp
@@ -37,7 +37,8 @@ int open_ts_input() {
 
                 fd = open(absolute_path, O_RDWR);
                 if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
-                    if (strcmp(name, "fts") == 0 || strcmp(name, "goodix_ts") == 0)
+                    if (strcmp(name, "fts") == 0 || strcmp(name, "goodix_ts") == 0 ||
+                            strcmp(name, "NVTCapacitiveTouchScreen") == 0)
                         break;
                 }
 

AOSPA-XD/android_device_xiaomi_violet

power/power-mode.cpp
@@ -37,7 +37,8 @@ int open_ts_input() {
 
                 fd = open(absolute_path, O_RDWR);
                 if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
-                    if (strcmp(name, "fts_ts") == 0 || strcmp(name, "goodix_ts") == 0)
+                    if (strcmp(name, "fts_ts") == 0 || strcmp(name, "goodix_ts") == 0 ||
+                            strcmp(name, "NVTCapacitiveTouchScreen") == 0)
                         break;
                 }
 